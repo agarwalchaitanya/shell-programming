8d412a320a94c63c653b6b09042aab453ecab000
agarwalchaitanya <00agchaitanya@gmail.com>
2019-02-15 19:04:27 +0530

    update 2

--- 2.sh
+++ 2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 touch -t $2 ./.dev.null1
 find $1 -newer ./.dev.null1 > ./.dev.null2
 mkdir archive-"$2"
 while read -r line
 do
-    mv $line archive-"$2"
+    mv $line ./archive-"$2"/
 done < ./.dev.null2
 
-rm .dev.null1 .dev.null2
+rm ./.dev.null1 ./.dev.null2
